55e9546625bce14bea301bdebd9dc3d42de79b26
Ross McLean <ross_mclean@posteo.net>
2021-03-07 22:53:26 +0000

    Create tracker router

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const path = require('path')
-const mustache = require('mustache-express')
 const app = express()
 
+const mustache = require('mustache-express')
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
 
+const path = require('path')
 const public = path.join(__dirname, 'public')
 app.use(express.static(public))
+
+const router = require('./routes/trackerRoutes')
+app.use('/', router)
 
 const PORT = process.env.port || 3000
 app.listen(PORT, () => {
