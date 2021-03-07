119020900594cd7a219c1001079df839d225117d
Ross McLean <ross_mclean@posteo.net>
2021-03-07 22:46:24 +0000

    add path npm package (again)
    Update index.js
    - set static content directory

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const app = express()
 
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
+
+const public = path.join(__dirname, 'public')
+app.use(express.static(public))
+
+const PORT = process.env.port || 3000
+app.listen(PORT, () => {
+  console.log(`Node server started on port ${PORT} - Press ctrl^c to quit.`)
+})
+
