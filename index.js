d633a0653b905bab4f9fbfe9a823d957bcf09cde
Ross McLean <ross_mclean@posteo.net>
2021-03-10 20:39:35 +0000

    Remove body-parser

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 const mustache = require('mustache-express')
 app.engine('mustache', mustache())
