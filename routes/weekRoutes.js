03e84e4a6723253b31ad80b6fa838d33fc3fb5fa
Ross McLean <ross_mclean@posteo.net>
2021-03-11 23:04:48 +0000

    Delete all references to week and user models

--- routes/weekRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const controller = require('../controllers/weekController')
-const router = express.Router()
-
-router.get('/', controller.get_all_weeks)
-// router.get('/week', controller.go_to_current_week)
-
-module.exports = router
