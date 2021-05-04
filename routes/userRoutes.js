2b28edb4d3c14208a6779063789bc84d0e49374d
Ross McLean <ross_mclean@posteo.net>
2021-05-04 21:29:13 +0100

    Update userRoutes.js
    - Point to correct controller
    - Add show login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const controller = require('../controllers/goalController')
+const controller = require('../controllers/userController')
 const router = express.Router()
 
 router.get('/register', controller.show_register_view)
+
+router.get('/login', controller.show_login_view)
 
 module.exports = router
