8a2df627fbc87cdd3b319ed91780da6832d708d0
Ross McLean <ross_mclean@posteo.net>
2021-05-05 18:02:54 +0100

    Add demo code to userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,67 @@
+const Datastore = require("nedb");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+class UserDAO {
+  constructor(dbFilePath) {
+    if (dbFilePath) {
+      //embedded
+      this.db = new Datastore({ filename: dbFilePath, autoload: true });
+    } else {
+      //in memory
+      this.db = new Datastore();
+    }
+  }
+
+  // for the demo the password is the bcrypt of the user name
+  init() {
+    this.db.insert({
+      user: 'Peter',
+      password: '$2b$10$I82WRFuGghOMjtu3LLZW9OAMrmYOlMZjEEkh.vx.K2MM05iu5hY2C'
+    });
+    console.log('user record inserted in init');
+
+    this.db.insert({
+      user: 'Ann',
+      password: '$2b$10$bnEYkqZM.MhEF/LycycymOeVwkQONq8kuAUGx6G5tF9UtUcaYDs3S'
+    });
+
+
+    console.log('user record inserted in init');
+    return this;
+  }
+
+  create(username, password) {
+    const that = this;
+    bcrypt.hash(password, saltRounds).then(function(hash) {
+      var entry = {
+        user: username,
+        password: hash,
+      };
+      console.log('user entry is: ', entry);
+
+      that.db.insert(entry, function (err) {
+        if (err) {
+          console.log("Can't insert user: ", username);
+        }
+      });
+    });
+  }
+
+  lookup(user, cb) {
+    this.db.find({'user': user}, function (err, entries) {
+      if (err) {
+        return cb(null, null);
+      } else {
+        if (entries.length == 0) {
+          return cb(null, null);
+        }
+        return cb(null, entries[0]);
+      }
+    });
+  }
+}
+
+const dao = new UserDAO();
+dao.init();
+module.exports = dao;
