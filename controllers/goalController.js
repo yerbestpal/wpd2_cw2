38b40a2f0588871bd17472d1d729d9f19a94e125
Ross McLean <ross_mclean@posteo.net>
2021-05-05 01:58:17 +0100

    Fix root redirect in goalController.js

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -8,7 +8,7 @@ const Moment = require('moment')
 
 exports.redirect_root_to_current_week = (req, res) => {
   const currentWeek = Moment().isoWeek()
-  res.redirect(`/${currentWeek}`)
+  res.redirect(`/goals/${currentWeek}`)
 }
 
 exports.get_all_goals = async (req, res) => {
